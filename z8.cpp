cbd36505cb2f83746f4c080be9d84ee07aaa23b2
virtual photon <zphoton0@gmail.com>
2020-09-14 20:32:52 +0300

    restored z8

--- z8.cpp
+++ z8.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <string>
 
-using namespace std
-void recur_deeper(string output, int len_of_output, int depth, int max_depth){
-	//include this value
+using namespace std;
+void recur_deeper(string output, int depth, int max_depth, int output_length) {
+	//border cases
 	{
-		output += string(depth);
+		if (depth == 0)
+			output = "{";
+
+		if (depth == max_depth) {
+			//delete last comma
+			if (output_length)
+				output = output.substr(0, output.length() - 2);
+
+			output = output.append("}");
+			cout << output << '\n';
+			return;
+		}
+	}
+	//do recursion
+	{
+
+		//not include
+		recur_deeper(output, depth + 1, max_depth, output_length + 1);
+
+		//include current element
+		output = output.append(to_string(depth).append(", "));
+		recur_deeper(output, depth + 1, max_depth, output_length + 1);
 
 	}
+
 }
 
-int main(){
+int main() {
 	int n;
 	cin >> n;
-	
+	recur_deeper("", 0, n, 0);
 	return 0;
 }
