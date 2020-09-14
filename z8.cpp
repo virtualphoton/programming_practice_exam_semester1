9f33f39c6bd47cf60e91d3badb3232420ae71bca
virtual photon <zphoton0@gmail.com>
2020-09-14 20:04:32 +0300

    added working version of problem 8

--- z8.cpp
+++ z8.cpp
@@ -2,27 +2,39 @@
 #include <string>
 
 using namespace std;
-void recur_deeper(string output, int depth, int max_depth) {
-    //border cases
-    {
-        if (depth == 0)
-            output = "{";
-        if (depth == max_depth) {
-            output += "}";
-            cout << output << '\n';
-            return;
-        }
-    }
-    //include current element
-    {
-        output = output.append(to_string(depth));
-        cout << output;
-    }
+void recur_deeper(string output, int depth, int max_depth, int output_length) {
+	//border cases
+	{
+		if (depth == 0)
+			output = "{";
+
+		if (depth == max_depth) {
+			//delete last comma
+			if (output_length)
+				output = output.substr(0, output.length() - 2);
+
+			output = output.append("}");
+			cout << output << '\n';
+			return;
+		}
+	}
+	//do recursion
+	{
+
+		//not include
+		recur_deeper(output, depth + 1, max_depth, output_length + 1);
+
+		//include current element
+		output = output.append(to_string(depth).append(", "));
+		recur_deeper(output, depth + 1, max_depth, output_length + 1);
+
+	}
+
 }
 
 int main() {
-    int n;
-    cin >> n;
-    recur_deeper("", 0, 10);
-    return 0;
+	int n;
+	cin >> n;
+	recur_deeper("", 0, n, 0);
+	return 0;
 }
