281310724ee290d454f4c4e1d816486fcdeddf30
virtual photon <zphoton0@gmail.com>
2020-09-15 10:16:41 +0300

    added working 29

---
+++ z29.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int global_iter = 1;
+	for (int i = 0; i < n; i += 1) {
+		for (int j = 0; j < n; j += 1) {
+			cout << global_iter << '\t';
+			global_iter += 1;
+		}
+		cout << '\n';
+	}
+
+}
