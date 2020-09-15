536eee5d8b4a678ed00bf8665a17bd3f4318e5db
virtual photon <zphoton0@gmail.com>
2020-09-15 08:38:14 +0300

    added working version of 10

---
+++ z10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string input;
+	cin >> input;
+	int current_mod = 0;
+	for (auto digit : input) {
+		current_mod *= 2;
+		if (digit == '1')
+			current_mod += 1;
+		current_mod %= 15;
+	}
+	cout << ((current_mod) ? "NO" : "YES");
+}
